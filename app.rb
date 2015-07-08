a42c7ce45f8d2dd6e45edf5a6e6f6654779f1f30
Adrian Kuhn <akuhn@iam.unibe.ch>
2015-07-07 22:20:07 -0700

    Less magic, more uppercase.

--- app.rb
+++ app.rb
@@ -5,45 +5,27 @@ require 'haml'
 
 module Bunnylol
 
-  @@commands = {}
-  @@help = {}
-  @@query = nil
+  COMMANDS = {}
+  HELP = {}
 
   def bunnylol(names, options = {}, &block)
     for name in names.map(&:to_s)
-      @@help[name] = options[:help]
-      @@commands[name] = block
+      HELP[name] = options[:help]
+      COMMANDS[name] = block
     end
-  end
-
-  def commands
-    @@commands
-  end
-
-  def help
-    @@help
-  end
-
-  def query= query
-    @@query = query.join(' ')
-  end
-
-  def query
-    @@query
   end
 
 end
 
-helpers Bunnylol
 register Bunnylol
 
 bunnylol %w(default google gg g), help: 'search on google' do
   base = 'https://www.google.com/search?q='
-  redirect base << CGI.escape(query)
+  redirect base << CGI.escape(@query)
 end
 
 bunnylol %w(help list h l), help: 'show list of commands' do
-  @help = help.sort_by { |name, help| name }
+  @help = Bunnylol::HELP.sort_by { |name, help| name }
   haml :help
 end
 
@@ -52,12 +34,12 @@ require './bunnylol' # include user defined commands
 get '/' do
   words = @params.fetch('q') { 'list' }.split
 
-  if commands.key? words.first
-    self.query = words.drop(1)
-    instance_exec &commands[words.first]
+  if Bunnylol::COMMANDS.key? words.first
+    @query = words.drop(1).join(' ')
+    instance_exec &Bunnylol::COMMANDS[words.first]
   else
-    self.query = words
-    instance_exec &commands['default']
+    @query = words.join(' ')
+    instance_exec &Bunnylol::COMMANDS['default']
   end
 end
 
