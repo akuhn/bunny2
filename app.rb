459b5ef5a42e2bdbbb1814c64c3c5a06c17c12c3
Adrian Kuhn <akuhn@iam.unibe.ch>
2018-04-17 21:11:12 -0700

    Remove coachme command.

--- app.rb
+++ app.rb
@@ -2,22 +2,18 @@ require 'sinatra'
 require 'haml'
 require 'json'
 
-# Edit lib folder to add more commands!
+require './lib/bunnylol'
 
-require './bunny'
 
 get '/' do
-
   @query = @params.fetch('q') { 'list' }
-
   command, arguments = @query.split(' ', 2)
 
-  if Bunny::COMMANDS.has_key? command
+  if Bunny::COMMANDS.has_key?(command)
     @query = arguments.to_s
   else
     command = 'default'
   end
 
   instance_exec &Bunny::COMMANDS[command]
-
 end
---
+++ lib/commands.rb
@@ -0,0 +1,45 @@
+# Smart commands for your browser
+
+create_table 'bookmarks', url: 'TEXT'
+
+bunnylol %w(bookmark book b), help: 'bookmark' do
+  sqlite 'INSERT INTO bookmarks (url) VALUES (?)', [@query] unless @query.empty?
+  @bookmarks = sqlite %(
+    SELECT created_at, url
+    FROM bookmarks
+    ORDER BY created_at
+    DESC LIMIT 40
+  )
+
+  haml :bookmarks
+end
+
+bunnylol %w(facebook fb), help: 'go to facebook profile' do
+  base = 'https://www.facebook.com/profile.php?id='
+  redirect base << CGI.escape(@query)
+end
+
+bunnylol %w(twitter tw t), help: 'open twitter' do
+  base = 'https://twitter.com'
+  redirect base unless @query
+  redirect base << '/search?q=' << CGI.escape(@query)
+end
+
+bunnylol %w{bing bb}, help: 'search with bing' do
+  base = 'https://bing.com'
+  redirect base unless @query
+  redirect base << '/search?q=' << CGI.escape(@query)
+end
+
+bunnylol %w{kitty k}, help: 'view cryptokitty' do
+  base = 'https://cryptokitties.co'
+  redirect base unless @query
+  kitten_number = @query.start_with?('0000') ? @query.to_i(16).to_s : @query
+  redirect base << '/kitty/' << CGI.escape(kitten_number)
+end
+
+bunnylol %w{dx docs}, help: 'search google documents' do
+  base = 'https://docs.google.com'
+  redirect base unless @query
+  redirect base << '?q=' << CGI.escape(@query)
+end
